f191cf34d335a3471f26b66cb39e3168cf323b4f
Xiaoshan Jiang <jasonxiaoshanjiang@gmail.com>
2022-08-15 21:05:53 +1000

    part-03-query-parameters

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, APIRouter
 
+from typing import Optional
+
 
 RECIPES = [
     {
@@ -23,7 +25,7 @@ RECIPES = [
 ]
 
 
-app = FastAPI(title="Receipe API", openapi_url="/openapi.json")
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
 
@@ -31,13 +33,11 @@ api_router = APIRouter()
 @api_router.get("/", status_code=200)
 def root() -> dict:
     """
-    Root Get
+    Root GET
     """
     return {"msg": "Hello, World!"}
 
 
-# New addition, path parameter
-# https://fastapi.tiangolo.com/tutorial/path-params/
 @api_router.get("/recipe/{recipe_id}", status_code=200)
 def fetch_recipe(*, recipe_id: int) -> dict:
     """
@@ -49,7 +49,27 @@ def fetch_recipe(*, recipe_id: int) -> dict:
         return result[0]
 
 
+# New addition, query parameter
+# https://fastapi.tiangolo.com/tutorial/query-params/
+@api_router.get("/search/", status_code=200)
+def search_recipes(
+    keyword: Optional[str] = None, max_results: Optional[int] = 10
+) -> dict:
+    """
+    Search for recipes based on label keyword
+    """
+    if not keyword:
+        # we use Python list slicing to limit results
+        # based on the max_results query parameter
+        return {"results": RECIPES[:max_results]}
+
+    results = filter(lambda recipe: keyword.lower()
+                     in recipe["label"].lower(), RECIPES)
+    return {"results": list(results)[:max_results]}
+
+
 app.include_router(api_router)
+
 
 if __name__ == "__main__":
     # Use this for debugging purposes only
