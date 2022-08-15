20424bfdeb91818d6da1a15ba874c6c2a501bf59
Xiaoshan Jiang <jasonxiaoshanjiang@gmail.com>
2022-08-15 17:20:43 +1000

    part-01-hello-world

---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, APIRouter
+
+app = FastAPI(title="Receipe API", openapi_url="/openapi.json")
+
+api_router = APIRouter()
+
+
+@api_router.get("/", status_code=200)
+def root() -> dict:
+    """
+    Root Get
+    """
+    return {"msg": "Hello, World!"}
+
+
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
